257510990c3f0b952d121c737f28eed99f1aa603
George Silva <georger.silva@gmail.com>
2015-06-26 14:55:45 -0300

    adding docs

--- src/project_name/settings/production.py
+++ src/project_name/settings/production.py
@@ -4,5 +4,5 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = []
 
-# removing the browsable API
+# removing the browsable API - comment this if you WANT the browsable API in production.
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ('rest_framework.renderers.JSONRenderer',)
