05c226dd256d2bd48b0e49dea0a6b035d98c4c94
George Silva <georger.silva@gmail.com>
2015-07-20 14:35:38 -0300

    adicionando suporte ao raven

--- src/project_name/settings/production.py
+++ src/project_name/settings/production.py
@@ -1,8 +1,22 @@
+import os
+import raven
 from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = []
+
+INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
+
+MIDDLEWARE_CLASSES = (
+  'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
+  'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware',
+) + MIDDLEWARE_CLASSES
+
+RAVEN_CONFIG = {
+    'dsn': '__dsn__',
+    'release': raven.fetch_git_sha(os.path.dirname(__file__))
+}
 
 # removing the browsable API - comment this if you WANT the browsable API in production.
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ('rest_framework.renderers.JSONRenderer',)
