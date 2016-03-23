29e5b614d5dd1aecb234f8210157c2b9b03a3594
George Silva <george@consultoriasigma.com.br>
2016-03-22 21:57:24 -0300

    adding django-dbbackup config

--- src/project_name/settings/production.py
+++ src/project_name/settings/production.py
@@ -1,3 +1,4 @@
+import os
 import raven
 from .base import *
 from .utils import get_env_variable
@@ -21,3 +22,10 @@ RAVEN_CONFIG = {
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ('rest_framework.renderers.JSONRenderer',)
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+
+# django dbbackup
+token_fp = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dbtoken")
+DBBACKUP_STORAGE = 'dbbackup.storage.dropbox_storage'
+DBBACKUP_TOKENS_FILEPATH = token_fp
+DBBACKUP_DROPBOX_APP_KEY = get_env_variable("DBBACKUP_DROP_APP_KEY")
+DBBACKUP_DROPBOX_APP_SECRET = get_env_variable("DBBACKUP_DROPBOX_APP_SECRET")
